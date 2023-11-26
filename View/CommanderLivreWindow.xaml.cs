b5c2d1dc076bbecee19fb51ff23286ee213495fd
sultanTrash <115166309+sultanTrash@users.noreply.github.com>
2023-11-26 16:53:58 -0500

    Init

--- View/CommanderLivreWindow.xaml.cs
+++ View/CommanderLivreWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ViewModel;
 
 namespace View
 {
@@ -19,9 +21,30 @@ namespace View
     /// </summary>
     public partial class CommanderLivreWindow : Window
     {
-        public CommanderLivreWindow()
+        public BibliothequeViewModel viewModel;
+        public CommanderLivreWindow(BibliothequeViewModel _viewModel)
         {
             InitializeComponent();
+            viewModel = _viewModel;
+            DataContext = viewModel;
+        }
+
+        private void confirmIsEnabled(object sender, TextChangedEventArgs e)
+        {
+            confirmerButton.IsEnabled = canConfirm();
+        }
+        private bool canConfirm()
+        {
+            if (!(String.IsNullOrWhiteSpace(TitreBox.Text)||
+                String.IsNullOrWhiteSpace(AuteurBox.Text)||
+                String.IsNullOrWhiteSpace(EditeurBox.Text)||
+                String.IsNullOrWhiteSpace(AnneeBox.Text)))
+            {
+                if (ISBNBox.Text.Length == 13 && Regex.IsMatch(ISBNBox.Text, @"^\d+$"))
+                {
+                    return true;
+                } else { return false; }
+            } else { return false; }
         }
     }
 }
